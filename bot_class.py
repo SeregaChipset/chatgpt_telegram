083eee58c95fb387a49afae5a759a5d85c7e2cda
SeregaChipset <l0k1@bk.ru>
2023-03-05 00:13:13 +0600

    каждый новый чат в своем экземпляре

--- bot_class.py
+++ bot_class.py
@@ -10,7 +10,8 @@ class ChBot:
     def __init__(self, token_tg, api_ai):
         self.bot = Bot(token=token_tg)
         self.dp = Dispatcher(self.bot)
-        self.ch_openai = ChChatGpt(api_ai)
+        self.api_ai = api_ai
+        self.chats = {}  # будущий список чатов
 
         self.dp.register_message_handler(self.start, commands=['start'])
         self.dp.register_message_handler(self.help, commands=['help'])
@@ -19,7 +20,7 @@ class ChBot:
 
         # добавляем кнопки
         help_button = types.KeyboardButton('/help')
-        empty_button = types.KeyboardButton('/debug')
+        empty_button = types.KeyboardButton('картинка ')
         self.custom_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True).row(help_button, empty_button)
 
     def del_name(self, stroka, temp_list):  # функция удаления обращения чмоня
@@ -31,6 +32,9 @@ class ChBot:
         return ' '.join(temp_list)
 
     async def start(self, message: types.Message):  # старт меню
+        chat_id = message.chat.id
+        if chat_id not in self.chats:
+            self.chats[chat_id] = ChChatGpt(self.api_ai)
         await message.reply(HELLO_MESSAGE, reply_markup=self.custom_keyboard)
 
     async def help(self, message: types.Message):  # хелп меню
@@ -39,22 +43,26 @@ class ChBot:
             await self.bot.send_photo(chat_id=message.chat.id, photo=types.InputFile(photo))
 
     async def debug(self, message: types.Message):  # получить ид чата + список сообщений
+        chat_id = message.chat.id
         await message.reply(message.chat.id)
-        await self.bot.send_message(message.chat.id, self.ch_openai.messages)
+        await self.bot.send_message(message.chat.id, self.chats[chat_id].messages)
 
     async def message(self, message: types.Message):  # функция обмена сообщениями
+        chat_id = message.chat.id
+        if chat_id not in self.chats:
+            self.chats[chat_id] = ChChatGpt(self.api_ai)
         msg_list = [slovo.strip('!.,%:\'\"\\/*;?') for slovo in message.text.lower().split()]  # создает список слов
-        if message.chat.type != "private":
-            text = self.ch_openai.out(message.text)  # запрашивает ответ у чатгпт
-            print(self.del_name(message.text, msg_list), text)  # отладка
+        if message.chat.type == "private":  # личная беседа
+            text = self.chats[chat_id].out(message.text)  # запрашивает ответ у чатгпт
+            print(message.text, text)  # отладка
             await self.bot.send_message(message.chat.id, text)  # отвечает
         elif NAME.lower() in msg_list:  # если зовут по имени (для групп)
-            text = self.ch_openai.out(self.del_name(message.text, msg_list))  # запрашивает ответ у чатгпт
+            text = self.chats[chat_id].out(self.del_name(message.text, msg_list))  # запрашивает ответ у чатгпт
             print(self.del_name(message.text, msg_list), text)  # отладка
             await self.bot.send_message(message.chat.id, text)  # отвечает
 
         if 'картинка' in msg_list:  # реагирует на слово картинки
-            input_io = self.ch_openai.out_image(message.text.replace('картинка', ''))  # запрос
+            input_io = self.chats[chat_id].out_image(message.text.replace('картинка', ''))  # запрос
             if input_io:
                 photo = types.InputFile(input_io, filename='image.png')  # возвращаем картинку в нужном для аиограм виде
                 await self.bot.send_photo(chat_id=message.chat.id, photo=photo)
